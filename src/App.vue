9500e261447f4a0027ee6a2e22a41b4d724294e1
anvarjon-programmer <mranvarjontojiboyev@gmail.com>
2024-09-01 11:41:31 +0300

    last commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>count{{ count }}</h1>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      count:0
+    }
+  },
+  methods:{
+    increment(){
+      this.count += 1
+    },
+    decrement(){
+      this.count -1
+    }
+  }
+}
+</script>
