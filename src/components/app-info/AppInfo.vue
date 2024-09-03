51a5e7ef8f56b95b58c9aa6b379eaf7f72e9ba2c
anvarjon-programmer <mranvarjontojiboyev@gmail.com>
2024-09-03 11:31:56 +0300

    add folder

---
+++ src/components/app-info/AppInfo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app-info">
+        <p class="fs-3 text-uppercase">barcha kinorlar soni:</p>
+        <p class="fs-4 text-uppercase">Koi'rilgan kinolar bsoni</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.app-info{
+    padding: 1.5rem;
+    border-radius: 4px;
+}
+</style>
